86809a0d1da0dfa187b9f782bc6a32b258333c03
elizabethh777 <elizabethh777@users.noreply.github.com>
2020-05-02 00:47:11 +0300

    Benefit

---
+++ HackCreativeGame/Assets/Scripts/Game/BenefitProvider.cs
@@ -0,0 +1,33 @@
+﻿using Game;
+using Game.Simulation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BenefitProvider : MonoBehaviour
+{
+    private List<Benefit> Benefits;
+    public void Add(Benefit benefit)
+    {
+        Benefits.Add(benefit);
+    }
+    [System.Serializable]
+    public class FlowGroupData
+    {
+        public FlowGroupKind target;
+        public float infectProbability;
+        public float happinessMultiplier;
+    }
+
+    public List<FlowGroupData> flowGroupData;
+    
+    public void CalculateFlowData()
+    {
+        foreach (var item in Benefits)
+        {
+
+        }
+    }
+}
+
+    

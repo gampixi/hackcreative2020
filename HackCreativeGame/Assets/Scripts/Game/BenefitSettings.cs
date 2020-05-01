74e33ebbca474bab7d541ef0e41cfff0e428cb77
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2020-05-02 00:07:27 +0300

    added benefit settings

---
+++ HackCreativeGame/Assets/Scripts/Game/BenefitSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Simulation;
+
+namespace Game
+{
+    [CreateAssetMenu(fileName = "NewBenefitSettings", menuName = "Custom Game Data/Benefit Settings", order = 1)]
+    public class BenefitSettings : ScriptableObject
+    {
+        [System.Serializable]
+        public class HappinessData
+        {
+            public FlowGroupKind target;
+            public float happinessMultiplier;
+        }
+        
+        public List<HappinessData> happinessData;
+        
+        [System.Serializable]
+        public class InfectProbabilityData
+        {
+            public FlowGroupKind target;
+            public float infectProbabilityMultiplier;
+        }
+        
+        public List<InfectProbabilityData> infectProbabilityData;
+    }
+}

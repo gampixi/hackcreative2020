355a93e76615c5eec78d7034f519b9e52b35bd8e
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2020-05-03 02:20:57 +0300

    cashflow indicator works

--- HackCreativeGame/Assets/Scripts/UI/UiMainHud.cs
+++ HackCreativeGame/Assets/Scripts/UI/UiMainHud.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Game.Simulation;
 using TMPro;
@@ -8,6 +9,8 @@ using static Game.Simulation.SimulationController;
 
 public class UiMainHud : MonoBehaviour
 {
+    public static UiMainHud Hud;
+    
     public Transform happiness;
     public Image happinessImage;
     public Color negativeColor;
@@ -18,12 +21,18 @@ public class UiMainHud : MonoBehaviour
     public TextMeshProUGUI infectedText;
     public TextMeshProUGUI deadText;
 
+    private void Awake()
+    {
+        Hud = this;
+    }
+
     private void Update()
     {
         happiness.localScale = new Vector3(Mathf.Clamp((float)Instance.happiness.happiness, -1f, 1f), 1, 1);
         happinessImage.color = Instance.happiness.happiness < 0 ? positiveColor : negativeColor;
         moneyText.text = Instance.money.Amount.ToString("N0");
-        moneyChangeText.text = $"aaaa1";
+        var flow = Instance.money.CalculateFutureSpending();
+        moneyChangeText.text = $"{(flow > 0 ? "+" : "")}{flow:N0}";
         aliveText.text = Instance.statistics.TotalAlive.ToString();
         infectedText.text = Instance.statistics.TotalSymptomatic.ToString();
         deadText.text = Instance.statistics.TotalDead.ToString();

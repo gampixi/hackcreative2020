240ccd22f37014373b15813c1ded8de0916e946f
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2020-05-02 13:24:04 +0300

    ui work started

---
+++ HackCreativeGame/Assets/Scripts/UI/UiBenefitWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiBenefitWindow : MonoBehaviour
+{
+    public GameObject listItemPrefab;
+    private List<GameObject> _currentList = new List<GameObject>();
+    
+    public void UpdateList()
+    {
+        
+    }
+}

997a1309930edf6e0f0d0ce0c50948b1bfa63872
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2020-05-02 12:07:22 +0300

    happiness base

---
+++ HackCreativeGame/Assets/Scripts/Simulation/Happiness.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game.Simulation;
+using UnityEngine;
+
+public class Happiness : MonoBehaviour
+{
+    public double happiness { get; private set; }
+
+    public void CalculateHappiness()
+    {
+        happiness = 0;
+        long totalPopulation = 0;
+        SimulationController.Instance.regions.ForEach(x => x.population.ForEach(p =>
+        {
+            happiness += p.GetHappiness();
+            totalPopulation += p.population.Total;
+        }));
+        happiness /= (double) totalPopulation;
+    }
+}

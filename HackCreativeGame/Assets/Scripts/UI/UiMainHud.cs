b0f8c2948340cd1032482347b2be3132d7fcbc31
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2020-05-02 20:25:18 +0300

    Prettier HUD

--- HackCreativeGame/Assets/Scripts/UI/UiMainHud.cs
+++ HackCreativeGame/Assets/Scripts/UI/UiMainHud.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using Game.Simulation;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using static Game.Simulation.SimulationController;
 
 public class UiMainHud : MonoBehaviour
 {
-    public TextMeshProUGUI happinessText;
+    public Transform happiness;
+    public Image happinessImage;
+    public Color negativeColor;
+    public Color positiveColor;
     public TextMeshProUGUI moneyText;
-    public TextMeshProUGUI populationText;
+    public TextMeshProUGUI moneyChangeText;
+    public TextMeshProUGUI aliveText;
+    public TextMeshProUGUI infectedText;
+    public TextMeshProUGUI deadText;
 
     private void Update()
     {
-        happinessText.text = $"HAPPINESS: {Instance.happiness.happiness}";
-        moneyText.text = $"0€";
-        populationText.text = $"Alive: {Instance.statistics.TotalAlive} Inf: {Instance.statistics.TotalSymptomatic} Dead: {Instance.statistics.TotalDead}";
+        happiness.localScale = new Vector3(Mathf.Clamp((float)Instance.happiness.happiness, -1f, 1f), 1, 1);
+        happinessImage.color = Instance.happiness.happiness < 0 ? positiveColor : negativeColor;
+        moneyText.text = $"001";
+        moneyChangeText.text = $"aaaa1";
+        aliveText.text = Instance.statistics.TotalAlive.ToString();
+        infectedText.text = Instance.statistics.TotalSymptomatic.ToString();
+        deadText.text = Instance.statistics.TotalDead.ToString();
     }
 
     public void NextDayButton()

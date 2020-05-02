0c63ba37b6a8f18f611bbf0cf0b95b939b986389
Artjoms00 <62503705+Artjoms00@users.noreply.github.com>
2020-05-02 14:18:34 +0300

    Optimize Statistics

--- HackCreativeGame/Assets/Scripts/Simulation/SimulationController.cs
+++ HackCreativeGame/Assets/Scripts/Simulation/SimulationController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -8,6 +9,7 @@ namespace Game.Simulation
 {
     public class SimulationController : MonoBehaviour
     {
+        private PopulationStatistics statistics = new PopulationStatistics();
         public static SimulationController Instance;
         public List<RegionNode> regions;
         public BenefitProvider benefits;
@@ -45,6 +47,34 @@ namespace Game.Simulation
             });
             happiness.CalculateHappiness();
         }
+
+        public class PopulationStatistics : MonoBehaviour
+        {
+            public int TotalRecovered { get; set; }
+            public int TotalHealthy { get; set; }
+            public int TotalSymptomatic { get; set; }
+            public long TotalAlive => TotalHealthy + TotalSymptomatic + TotalRecovered;
+            public long TotalDead { get; set; }
+        }
+
+        public PopulationStatistics GetStatistics()
+        {
+            statistics.TotalHealthy = regions.Sum(region => region
+                .population.Sum(people => people.population.healthy + people.population.asymptomatic));
+
+            statistics.TotalSymptomatic = regions.Sum(region => region
+               .population.Sum(people => people.population.symptomatic));
+
+            statistics.TotalRecovered = regions.Sum(region => region
+               .population.Sum(people => people.population.recovered));
+
+            var totalInitial = regions.Sum(x => x.settings.initialPopulation);
+
+            statistics.TotalDead = totalInitial - statistics.TotalAlive;
+
+            return statistics;
+        }
+
     }
 }
 
